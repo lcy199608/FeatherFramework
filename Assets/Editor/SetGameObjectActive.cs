174dc4fff836fead5a3086eca9ed283e0d29f616
lcy <1041524326@qq.com>
2020-07-11 11:34:14 +0800

    快捷键和pool优化

---
+++ Assets/Editor/SetGameObjectActive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SetGameObjectActive : Editor
+
+{
+    [MenuItem("Tools/通用工具/切换物体显隐状态 %e")]
+    public static void SetObjActive()
+    {
+        GameObject[] selectObjs = Selection.gameObjects;
+        int objCtn = selectObjs.Length;
+        for (int i = 0; i < objCtn; i++)
+        {
+            bool isAcitve = selectObjs[i].activeSelf;
+            selectObjs[i].SetActive(!isAcitve);
+        }
+    }
+}
--- Assets/Scripts/Test/PoolTest.cs
+++ Assets/Scripts/Test/PoolTest.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PoolTest : MonoBehaviour
 {
+    private Transform parent;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            PoolMgr.Instance.GetObj("Test");
+            PoolMgr.Instance.GetObj("Image", _ => {  _.transform.parent = parent == null ? parent = GameObject.Find("Canvas").transform : parent;_.transform.localPosition = Vector3.zero; });
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            PoolMgr.Instance.GetObj("Test1");
+            PoolMgr.Instance.GetObj("Test1", _ => { });
         }
     }
 }
