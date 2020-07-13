df359badc03a30adcbe71c902025adc4826210e0
lcy <1041524326@qq.com>
2020-07-13 18:05:09 +0800

    增加多语言模块

---
+++ Assets/Scripts/FeatherFramework/LanguageMgr/GameLanguageManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class GameLanguageManager : Singleton<GameLanguageManager>
+{
+    public GameLanguageManager()
+    {
+        currentLanguage = SaveHandler.GetSystemData(languageFile, currentLanguage);
+    }
+
+    SupportedLanguage? currentLanguage;
+    const string  languageFile = "LanguageFile";
+    public enum SupportedLanguage { Chinese, English, Portuguese, Spanish, French, Japanese, Korean, German }
+
+    public SupportedLanguage CurrentLanguage
+    {
+        get
+        {
+            if (currentLanguage == null)
+            {
+                if (Application.systemLanguage == SystemLanguage.Chinese || Application.systemLanguage == SystemLanguage.ChineseSimplified || Application.systemLanguage == SystemLanguage.ChineseTraditional)
+                {
+                    currentLanguage = SupportedLanguage.Chinese;
+                }
+                else if (Application.systemLanguage == SystemLanguage.English)
+                {
+                    currentLanguage = SupportedLanguage.English;
+                }
+                //else if (Application.systemLanguage == SystemLanguage.Portuguese)
+                //{
+                //    currentLanguage = SupportedLanguage.Portuguese;
+                //}
+                //else if (Application.systemLanguage == SystemLanguage.Spanish)
+                //{
+                //    currentLanguage = SupportedLanguage.Spanish;
+                //}
+                //else if (Application.systemLanguage == SystemLanguage.French)
+                //{
+                //    currentLanguage = SupportedLanguage.French;
+                //}
+                //else if (Application.systemLanguage == SystemLanguage.Japanese)
+                //{
+                //    currentLanguage = SupportedLanguage.Japanese;
+                //}
+                //else if (Application.systemLanguage == SystemLanguage.Korean)
+                //{
+                //    currentLanguage = SupportedLanguage.Korean;
+                //}
+                //else if (Application.systemLanguage == SystemLanguage.German)
+                //{
+                //    currentLanguage = SupportedLanguage.German;
+                //}
+                else
+                {
+                    currentLanguage = SupportedLanguage.Chinese;
+                }
+            }
+            currentLanguage = SaveHandler.GetSystemData(languageFile, currentLanguage);
+            return currentLanguage.Value;
+        }
+
+        set { SaveHandler.SetSystemData(languageFile, value); }
+    }
+}
---
+++ Assets/Scripts/FeatherFramework/LanguageMgr/MLT_GameObjectSwitch.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class MLT_GameObjectSwitch : MonoBehaviour
+{
+    public GameObject CN, EN, Portuguese, Spanish, French, Japanese, Korean, German;
+    private void Awake()
+    {
+        Switch();
+
+        EventCenter.Instance.AddEventListener("语言切换", Switch);
+    }
+
+    void Switch()
+    {
+        CN?.SetActive(false);
+        EN?.SetActive(false);
+        Portuguese?.SetActive(false);
+        Spanish?.SetActive(false);
+        French?.SetActive(false);
+        Japanese?.SetActive(false);
+        Korean?.SetActive(false);
+        German?.SetActive(false);
+
+        switch (GameLanguageManager.Instance.CurrentLanguage)
+        {
+            case GameLanguageManager.SupportedLanguage.Chinese:
+                CN?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.English:
+                EN?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.Portuguese:
+                Portuguese?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.Spanish:
+                Spanish?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.French:
+                French?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.Japanese:
+                Japanese?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.Korean:
+                Korean?.SetActive(true);
+                break;
+            case GameLanguageManager.SupportedLanguage.German:
+                German?.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        EventCenter.Instance.RemoveEventListener("语言切换", Switch);
+    }
+}
