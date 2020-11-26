a5f4873df1f29c1af171196b6e74ee7139b421bf
lcy <1041524326@qq.com>
2020-11-27 00:33:16 +0800

    111

--- Assets/Editor/SetGameObjectActive.cs
+++ Assets/Editor/SetGameObjectActive.cs
@@ -6,7 +6,7 @@ using System;
 public class SetGameObjectActive : Editor
 
 {
-    [MenuItem("Tools/通用工具/切换物体显隐状态 %e")]
+    [MenuItem("LcyFramework/通用工具/切换物体显隐状态 %e")]
     public static void SetObjActive()
     {
         GameObject[] selectObjs = Selection.gameObjects;
